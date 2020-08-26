eb3a24413396a83c2162c858c192c3388fcbecb1
Nicholas Balby Moreno <nicholas.balby@academico.ifrn.edu.br>
2020-08-26 17:26:03 -0300

    Pastas estáticas(front) prontas para receber arquivos. Configuração para upload de imagens completa. Falta configurar arquivos URLs para renderizar front.

--- CookIt/urls.py
+++ CookIt/urls.py
@@ -20,4 +20,4 @@ from django.conf.urls.static import static  # Para upload de imagens
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #Para upload de imagens
