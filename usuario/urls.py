446ef54fb79e3705a431165ca8655f138ffa5c11
Colais <nicholasbalby@hotmail.com>
2020-09-08 21:59:12 -0300

    Telas de favoritos de cadastro de usuario incluidas

--- usuario/urls.py
+++ usuario/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('editar', views.editarPerfil, name='editarPerfil')
+    path('editar', views.editarPerfil, name='editarPerfil'),
+    path('favoritos', views.favoritos, name='favoritos'),
+    path('cadastro1', views.cadastro_usuario1, name='favoritos'),
+    # TODO: lembrar de deixar as duas telas de cadastro em apenas uma URL
+    path('cadastro2', views.cadastro_usuario2, name='favoritos'),
 ]
