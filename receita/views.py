c47e7094188ed6ad1ec25e1bc50048af847d679d
Thales <t.azevedo@escolar.ifrn.edu.br>
2020-09-24 18:34:15 -0300

    Renderizando receitas

--- receita/views.py
+++ receita/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from . import models
 
+#TODO: paginate_by
+class ListarReceita(ListView):
+    model = models.Receita
+    template_name = 'receita/index.html'
+    context_object_name = 'receitas'
 
-def index(request):
-    return render(request, 'receita/index.html', {
-    })
+#TODO: DetailView
+
