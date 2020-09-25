f658de203499767a957208373a8beb6cb8c2ea35
Filipe de Oliveira Ataíde <filipe.ataide@academico.ifrn.edu.br>
2020-09-25 18:06:33 -0300

    Paginação

--- receita/views.py
+++ receita/views.py
@@ -3,11 +3,11 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from . import models
 
-#TODO: paginate_by
 class ListarReceita(ListView):
     model = models.Receita
     template_name = 'receita/index.html'
     context_object_name = 'receitas'
+    paginate_by = 3
 
 #TODO: DetailView
 
