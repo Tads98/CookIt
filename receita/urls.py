8eea75ab83be213dbdf3aa9e5aa1b477a2150950
Filipe de Oliveira Ataíde <filipe.ataide@academico.ifrn.edu.br>
2020-09-28 16:57:20 -0300

    Adição da tela de receita completa

--- receita/urls.py
+++ receita/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ListarReceita.as_view(), name='index')
+    path('', views.ListarReceita.as_view(), name='index'),
 ]
