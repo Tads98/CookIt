cf4d8aeb473cdd0193a5448c6ff3521af9ce8ed3
Filipe de Oliveira Ataíde <filipe.ataide@academico.ifrn.edu.br>
2020-09-29 17:21:15 -0300

    Resolução de elementos na página de receita, adição de link na logo do navbar e redesign da página inicial

--- receita/urls.py
+++ receita/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'receita'
+
 urlpatterns = [
     path('', views.ListarReceita.as_view(), name='index'),
     path('<slug>', views.DetalheReceita.as_view(), name='receita_completa'),
