27011875db3a0c373db176ff745da128910b024c
VICTOR AUGUSTO FERNANDES PEREIRA <victor.augusto@academico.ifrn.edu.br>
2020-10-07 17:44:43 -0300

    alterando: telas de cadastro, add javascript e id no css

---
+++ core/static/custom/js/login_cadastro.js
@@ -0,0 +1,12 @@
+function loginOpen() {
+    document.getElementById("login").style.display = "inline-block";
+    document.getElementById("cadastro").style.display = "none";
+}
+  
+function cadastroOpen() {
+    document.getElementById("login").style.display = "none";
+    document.getElementById("cadastro").style.display = "inline-block";
+    
+}
+  
+  
