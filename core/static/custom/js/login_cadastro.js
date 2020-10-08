40555132fb2e0aafdfb92d3dbfdc7a0927f97fc8
Filipe de Oliveira Ataíde <filipe.ataide@academico.ifrn.edu.br>
2020-10-07 21:02:59 -0300

    Redesign da Tela de Login/Cadastro

--- core/static/custom/js/login_cadastro.js
+++ core/static/custom/js/login_cadastro.js
@@ -1,12 +1,14 @@
 function loginOpen() {
-    document.getElementById("login").style.display = "inline-block";
-    document.getElementById("cadastro").style.display = "none";
+    document.getElementById("login-form").style.display = "block";
+    document.getElementById("cadastro-form").style.display = "none";
+    document.getElementById("login-button").style.backgroundColor = "gray";
+    document.getElementById("cadastro-button").style.backgroundColor = "lightgray";
 }
-  
+
 function cadastroOpen() {
-    document.getElementById("login").style.display = "none";
-    document.getElementById("cadastro").style.display = "inline-block";
-    
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("cadastro-form").style.display = "block";
+    document.getElementById("login-button").style.backgroundColor = "lightgray";
+    document.getElementById("cadastro-button").style.backgroundColor = "gray";
+
 }
-  
-  
