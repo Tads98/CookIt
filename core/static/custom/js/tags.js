28d1f3ec98b3bb02c56d21b6573e2827a2cf825e
Filipe de Oliveira Ataide <filipe.ataide@escolar.ifrn.edu.br>
2020-12-01 19:00:08 -0300

    Adição de arquivo tags.js

---
+++ core/static/custom/js/tags.js
@@ -0,0 +1,56 @@
+const tagContainer = document.querySelector(".tag-container");
+const input = document.querySelector(".tag-container input");
+var tags = [];
+
+function createTag(label) {
+    const div = document.createElement('div');
+    div.setAttribute('class', 'tag btn-primary');
+    const span = document.createElement('span');
+    span.innerHTML = label;
+    span.setAttribute('class', 'mr-1');
+    const close = document.createElement('i');
+    close.setAttribute('class', 'fas fa-times');
+    close.setAttribute('data-item', label);
+    div.appendChild(span);
+    div.appendChild(close);
+    const input = document.createElement('input');
+    input.setAttribute('type', 'hidden');
+    input.setAttribute('value', label);
+    input.setAttribute('name', 'search');
+    div.appendChild(input);
+    return div;
+}
+
+function resetTags() {
+    document.querySelectorAll('.tag').forEach(function(tag) {
+        tag.parentElement.removeChild(tag);
+    })
+}
+
+function addTags() {
+    resetTags();
+    tags.slice().reverse().forEach(function(tag) {
+        const input = createTag(tag);
+        tagContainer.prepend(input);
+    })
+}
+
+input.addEventListener('keyup', function addTag(e) {
+    if (e.key == 'Space') {
+        str = input.value.slice(0, -1);
+        Console.log(str);
+        tags.push(str);
+        addTags();
+        input.value = '';
+    }
+});
+
+document.addEventListener('click', function removeTag(e) {
+    if (e.target.tagName == 'I') {
+        const value = e.target.getAttribute('data-item');
+        const index = tags.indexOf(value);
+        console.log(index);
+        tags.splice(index, 1)
+        addTags();
+    }
+})
