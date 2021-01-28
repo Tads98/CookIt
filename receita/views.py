fc9439248e0909e7fb99f4e7edb2c15ea1514a53
Colais <nicholasbalby@hotmail.com>
2021-01-28 08:31:11 -0300

    Adicionando simplificação de cadastro de receita.

--- receita/views.py
+++ receita/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.db.models import Q
 from . import models
-from .forms import ReceitaForm, IngredienteForm
+from .forms import ReceitaForm
 
 
 class ListarReceita(ListView):
@@ -21,12 +21,16 @@ class ListarReceita(ListView):
 
 class Busca(ListarReceita):
     def get_queryset(self, *args, **kwargs):
-        termo = self.request.GET.get('termo') or self.request.session.get('termo', None)
-        sabor = self.request.GET.getlist('sabor') or self.request.session.get('sabor', None)
-        dificuldade = self.request.GET.get('dificuldade') or self.request.session.get('dificuldade', None)
-        porcoes = self.request.GET.get('porcoes') or self.request.session.get('porcoes', None)
-        ingredientes = self.request.GET.getlist('ingredientes') or self.request.session.get('ingredientes', None)
-         
+        termo = self.request.GET.get(
+            'termo') or self.request.session.get('termo', None)
+        sabor = self.request.GET.getlist(
+            'sabor') or self.request.session.get('sabor', None)
+        dificuldade = self.request.GET.get(
+            'dificuldade') or self.request.session.get('dificuldade', None)
+        porcoes = self.request.GET.get(
+            'porcoes') or self.request.session.get('porcoes', None)
+        ingredientes = self.request.GET.getlist(
+            'ingredientes') or self.request.session.get('ingredientes', None)
 
         self.request.session['busca']['termo'] = termo
         self.request.session['busca']['sabor'] = sabor
@@ -35,7 +39,7 @@ class Busca(ListarReceita):
         self.request.session['busca']['ingredientes'] = ingredientes
 
         qs = super().get_queryset(*args, **kwargs)
-        #TODO: fazer com assentos
+        # TODO: fazer com assentos
         if termo:
             qs = qs.filter(
                 Q(nome_receita__icontains=termo)
@@ -64,11 +68,13 @@ class Busca(ListarReceita):
         self.request.session.save()
         return qs
 
+
 class Limpar(View):
     def get(self, *args, **kwargs):
-        #TODO: clear deslogando usuario, concertar 
+        # TODO: clear deslogando usuario, concertar
         self.request.session.clear()
         return redirect('receita:index')
+
 
 class DetalheReceita(DetailView):
     model = models.Receita
@@ -78,11 +84,12 @@ class DetalheReceita(DetailView):
 
 
 class CadastrarReceita(LoginRequiredMixin, CreateView):
+    # TODO: dar um jeito enviar uma mensagem de aviso para o usuário via 'messages'
     login_url = 'usuario:criar'
     template_name = 'receita/teste.html'
     model = models.Receita
     form_class = ReceitaForm
-    
+
     def get_success_url(self):
         return reverse('receita:index')
 
