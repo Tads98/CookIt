e31f7c1f58c506b0b8e43f0e2103b9ec59ec2f51
Filipe de Oliveira Ataide <filipe.ataide@escolar.ifrn.edu.br>
2021-03-09 21:38:10 -0300

    Implementando o Vue

---
+++ front-end/src/components/parciais/Navbar.vue
@@ -0,0 +1,186 @@
+<template>
+  <nav class="navbar navbar-expand-lg d-flex justify-content-between fixed-top">
+    <a class="navbar-brand" href="{% url 'receita:limpar' %}">
+      <img src="{% static 'imgs/cookit_logo.svg' %}" alt="Logo" height="36px" />
+    </a>
+    <form
+      class="form-inline <!--mr-auto my-2 my-lg-0-->"
+      method="GET"
+      action="{% url 'receita:busca' %}"
+    >
+      <div id="search" class="input-group">
+        <!--TODO: implementar uma forma de limpar sessões neste campo abaixo-->
+        <input
+          id="search-input"
+          class="form-control"
+          type="search"
+          placeholder="Busque sua receita"
+          aria-label="Busca"
+          name="termo"
+          value="{{ request.GET.termo }}"
+        />
+        <button id="search-button" class="btn btn-success" type="submit">
+          <i class="fas fa-search"></i>
+        </button>
+      </div>
+    </form>
+
+    <div class="" id="navbarSupportedContent">
+      <ul class="nav">
+        {% csrf_token %} {% if request.user.is_authenticated %}
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link"
+            href="{% url 'receita:cadastrar_receita' %}"
+            role="button"
+          >
+            <h5><i style="font-size: 25px" class="fas fa-book"></i></h5>
+          </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <h5><i style="font-size: 25px" class="fas fa-bell"></i></h5>
+          </a>
+          <div
+            class="dropdown-menu dropdown-menu-right"
+            aria-labelledby="navbarDropdown"
+          >
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a id="nav-icon" class="dropdown-item" href="#"
+              >Something else here</a
+            >
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <h5><i style="font-size: 25px" class="fas fa-heart"></i></h5>
+          </a>
+          <div
+            class="dropdown-menu dropdown-menu-right"
+            aria-labelledby="navbarDropdown"
+          >
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Something else here</a>
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a
+            class="nav-link"
+            href="#"
+            id="navbarDropdown"
+            role="button"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            <img
+              src="{% static 'imgs/cookit_user.svg' %}"
+              alt=""
+              height="25px"
+            />
+          </a>
+
+          <div
+            class="dropdown-menu dropdown-menu-right"
+            aria-labelledby="navbarDropdown"
+          >
+            <a class="dropdown-item" href="{% url 'usuario:criar' %}">Perfil</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="{% url 'usuario:logout' %}">Sair</a>
+          </div>
+        </li>
+        <html>
+          {% else %}
+          <button
+            id="login-button"
+            class="btn btn-primary"
+            onclick="location.href = '{% url 'usuario:criar' %}';loginOpen()"
+          >
+            Login
+          </button>
+          <!--<button id="cadastro-button" class="btn btn-secondary"
+                onclick="location.href = '{% url 'usuario:criar' %}';cadastroOpen()">
+                Cadastro
+            </button>-->
+        </html>
+      </ul>
+
+      {% endif %}
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.navbar {
+  background-color: #f3efef !important;
+  border-bottom: 1px solid #707070;
+}
+
+#nav-icon {
+  color: black;
+  vertical-align: middle;
+}
+
+.nav-link {
+  padding: 0 1vw 0 1vw;
+}
+
+.navbar-brand {
+  align-self: center;
+  justify-content: center;
+  font-size: 0;
+}
+
+#nav-icon-span {
+  font-size: 12px;
+}
+
+#search-input {
+  outline: none;
+  box-shadow: none !important;
+  border-radius: 36px 0 0 36px;
+  width: 20vw;
+}
+
+#search-button {
+  padding: 0 1em 0 1em;
+  background-color: white;
+  border: 1px solid gray;
+  color: gray;
+  margin-left: -1px;
+  border-radius: 0 36px 36px 0 !important;
+}
+
+#search-button i {
+  vertical-align: middle;
+}
+
+i {
+  text-decoration: none !important;
+  color: black;
+}
+</style>
---
+++ front-end/src/components/receita/FormReceita.vue
@@ -0,0 +1,214 @@
+<template>
+  <div class="container">
+    <div class="container">
+      <h3>Inserindo Sua Receita</h3>
+      {% include 'parciais/_messages.html' %}
+      <form enctype="multipart/form-data" method="POST">
+        {% csrf_token %}
+        <div class="form-group" id="form_receita_nome_receita">
+          <label>Nome da receita:</label>
+          {{ receitaform.nome_receita | as_crispy_field }}
+        </div>
+
+        <div class="form-group" id="form_receita_fotos">
+          <label for="inputAddress2">Fotos:</label>
+          {{ receitaform.fotos | as_crispy_field }}
+        </div>
+
+        <div class="form-row">
+          <div class="form-group col-md-6" id="form_receita_ingredientes">
+            <label for="inputPassword4">Ingredientes</label>
+            {{ ingredienteform.nome_ingrediente | as_crispy_field }}
+            {{ ingredienteform.unidade_medida_ingrediente | as_crispy_field }}
+            {{ ingredienteform.quantidade_ingrediente | as_crispy_field }}
+          </div>
+          <div
+            class="form-group col-md-6"
+            id="form_receita_caracteristicas_gerais"
+          >
+            <label for="inputPassword4">Características gerais</label>
+            <div id="form_receita_caracteristicas_gerais_container">
+              <div>
+                {{ receitaform.sabor_receita | as_crispy_field }}
+                {{ receitaform.categoria | as_crispy_field }}
+              </div>
+              <div>
+                <label for="porcoes">Porções</label>
+                {{ receitaform.porcoes | as_crispy_field }}
+
+                <label for="tempo_preparo">Tempo de preparo</label>
+                {{ receitaform.tempo_preparo | as_crispy_field }}
+
+                {{ receitaform.tempo_unidade_medida | as_crispy_field }}
+
+                {{ receitaform.dificuldade | as_crispy_field }}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="form-row">
+          <div class="form-group col-md-6" id="form_receita_modo-preparo">
+            <label for="modo_preparo">Modo de preparo</label>
+            {{ receitaform.modo_preparo | as_crispy_field }}
+          </div>
+
+          <div class="form-group col-md-6" id="form_receita_obs-adicionais">
+            <label for="inputPassword4">Observações adicionais</label>
+            {{ receitaform.observacoes_adicionais | as_crispy_field }}
+          </div>
+        </div>
+        <div class="form-row">
+          <button id="form_receita_button" type="submit" class="btn">
+            Postar
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.container {
+  border: none !important;
+  padding-top: 4rem;
+  padding-bottom: 4rem;
+  background-color: white;
+  border-left: 1px solid darkgray;
+  border-right: 1px solid darkgray;
+}
+
+#search-input-editarreceita {
+  outline: none;
+  box-shadow: none !important;
+  border-radius: 36px 0 0 36px;
+  width: 20vw;
+}
+
+#search-button-editarreceita {
+  border-radius: 0 36px 36px 0;
+  padding: 0 1em 0 1em;
+  background-color: white;
+  border: 1px solid gray;
+  color: gray;
+  margin-left: -1px;
+}
+
+#search-button-editarreceita i {
+  vertical-align: middle;
+}
+
+#thumbnail-input-group-editarreceita {
+  padding: 1em 0 1em 0 !important;
+}
+
+.jumbotron {
+  background-color: lightgray;
+  border: 1px solid gray;
+}
+
+#thumbnail-input-editarreceita {
+  list-style-type: none;
+  width: 10vw;
+  height: 10vw;
+  font-size: 4vw;
+  color: black;
+  border: 1px solid gray;
+}
+
+#thumbnail-input-editarreceita a {
+  color: black;
+}
+
+#prep-time-editarreceita,
+textarea,
+#portions-editarreceita {
+  border: none;
+  outline: none;
+  box-shadow: none !important;
+}
+
+#prep-input-editarreceita {
+  height: 60px;
+}
+
+#prep-input-editarreceita textarea {
+  min-width: 40px;
+  resize: none;
+}
+
+textarea {
+  width: 100%;
+  height: 100%;
+}
+
+h3,
+h4 {
+  font-weight: bold;
+  text-align: center;
+  margin-bottom: 1em;
+}
+
+.form-control,
+.input-group-text,
+.custom-control-input {
+  border: 1px solid gray !important;
+}
+
+#portions-editarreceita,
+#prep-time-editarreceita,
+#difficulty-editarreceita {
+  border-radius: 0 36px 36px 0;
+}
+
+.input-label-editarreceita {
+  border-radius: 36px 0 0 36px !important;
+}
+
+#submit-button-editarreceita {
+  background-color: white;
+  font-weight: bold;
+  border: 1px solid gray !important;
+  color: black;
+  margin: 0 !important;
+}
+
+#prep-button-editarreceita {
+  background-color: lightgray;
+  border: 1px solid gray !important;
+  color: black;
+}
+
+.form {
+  align-items: center;
+  justify-content: center;
+}
+
+#easy-editarreceita {
+  background-color: #b1ff62;
+}
+
+#medium-editarreceita {
+  background-color: #ffe132;
+}
+
+#hard-editarreceita {
+  background-color: #fc1d00;
+}
+
+#super-editarreceita {
+  background-color: #191919;
+}
+
+#prep-mode-editarreceita div div {
+  border-radius: 18px !important;
+}
+
+#prep-editarreceita .jumbotron {
+  height: 460px !important;
+}
+</style>
