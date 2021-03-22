ab0b81f02a0c67b35f7cf05626aca4cdca66d9f0
Filipe de Oliveira Ataide <filipe.ataide@escolar.ifrn.edu.br>
2021-03-22 20:47:16 -0300

    Implementando o VueRouter

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,36 +1,17 @@
 <template>
   <div>
-    <Navbar/>
-    <Index/>
-    <PaginaReceita/>
-    <FormReceita/>
+    <router-view/>
   </div>
 </template>
 
-<script>
-import Index from './components/Index.vue'
-import Navbar from './components/parciais/Navbar.vue'
-import PaginaReceita from './components/receitas/PaginaReceita.vue'
-import FormReceita from './components/receitas/FormReceita.vue'
-
+<script>/*
 export default {
   name: 'App',
   components: {
-    Index,
-    Navbar,
-    PaginaReceita,
-    FormReceita,
+    
   }
-}
+}*/
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
