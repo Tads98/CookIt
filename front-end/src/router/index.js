96fcc4bce70d7d5b23af93b05fc72c3e02fd0734
VICTOR AUGUSTO FERNANDES PEREIRA <victor.augusto@academico.ifrn.edu.br>
2021-03-22 22:27:28 -0300

    adicionar rotas do crud

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Index from '../components/Index.vue'
+import PaginaReceita from '../components/receitas/PaginaReceita'
 import FormReceita from '../components/receitas/FormReceita'
+import EditarPerfil from '../components/usuario/EditarPerfil'
+import LoginCadastro from '../components/usuario/LoginCadastro'
+
 
 Vue.use(VueRouter)
 
@@ -15,7 +19,22 @@ const routes = [
         path: '/FormReceita/',
         name: 'FormReceita',
         component: FormReceita
-    }
+    },
+    {
+        path: '/PaginaReceita/',
+        name: 'PaginaReceita',
+        component: PaginaReceita
+    },
+    {
+        path: '/EditarPerfil/',
+        name: 'EditarPerfil',
+        component: EditarPerfil
+    },
+    {
+        path: '/LoginCadastro/',
+        name: 'LoginCadastro',
+        component: LoginCadastro
+    },
 ]
 
 const router = new VueRouter({
