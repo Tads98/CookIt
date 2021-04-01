ea2e8e46e7787e40a75627399eb824b368953152
Thales Silva <thales.silva@parnamirim.rn.gov.br>
2021-04-01 16:37:53 -0300

    Incluindo campos ao detalhe de receita e arrumando caminho listagem para detalhe

--- back-end/CookIt/settings.py
+++ back-end/CookIt/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'CookIt.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -103,7 +103,7 @@ DATABASES = {
         'PORT': '',  # 8000 is default
     }
 }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
